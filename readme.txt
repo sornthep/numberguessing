478d793bd358b78f06586b57b49607293c14fdc3
Sornthep Vannarat <sornthep@Sorntheps-MacBook-Air.local>
2022-11-15 10:30:07 +0700

    Initial commit

---
+++ readme.txt
@@ -0,0 +1,8 @@
+This is a javascript based web app. An exercise from a javascript course. Consisting of 3 files: 
+- index.html
+- style.css 
+- script.js 
+
+The game: user try to guess a computer generated secret number (between 1 and maxi). 
+The user's score is decreased every wrong guess. When a correct guess is made, the score
+is finalized and the highest score is updated (kept until the app is closed.)
---
+++ script.js
@@ -0,0 +1,56 @@
+console.log('start...');
+
+const maxi = 50;
+
+let secret = Math.trunc(Math.random()*maxi) + 1;
+let yourGuess;
+let numScore = maxi;
+let highestScore = 0;
+let won = false;
+
+const msgEl = document.querySelector('.message');
+const guessEl = document.querySelector('.guess');
+const btnEl = document.querySelector('.btn');
+const score = document.querySelector('.score');
+const reset = document.querySelector('.reset-btn');
+
+myFunction = () => {
+    if (!won) {
+    yourGuess = Number(guessEl.value);
+    // console.log(guessEl.value,yourGuess);
+    if (yourGuess>=1 && yourGuess<=maxi) {
+        if (yourGuess>secret) {
+            msgEl.textContent = yourGuess + " is too high.  ";
+            numScore--;
+            score.textContent = `Your score: ${numScore}. Highest score: ${highestScore}.`;
+        } else if (yourGuess<secret) {
+            msgEl.textContent = yourGuess + " is too low.  ";
+            numScore--;
+            score.textContent = `Your score: ${numScore}. Highest score: ${highestScore}.`;
+        } else {
+            msgEl.textContent = yourGuess + " is correct.  ";
+            if (numScore>highestScore) highestScore = numScore;
+            score.textContent = `Your score: ${numScore}. Highest score: ${highestScore}.`;
+            won = true;
+        }
+    } else {
+        msgEl.textContent = `Please guess between 1 and ${maxi}.  `;
+    }
+} else {
+    msgEl.textContent = "You have won. Click reset to start a new game.";
+}
+
+}
+
+myResetFunction = () => {
+    secret = Math.trunc(Math.random()*maxi) + 1;
+    numScore = maxi;
+    guessEl.value = '';
+    msgEl.textContent = "Enter you guess here";
+    score.textContent = `Your score: ${numScore}. Highest score: ${highestScore}.`;
+    won = false;
+} 
+
+myResetFunction();
+btnEl.addEventListener("click", myFunction);
+reset.addEventListener("click", myResetFunction);
